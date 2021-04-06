f0b2f60e1247f319c7019fe0aa25e9a6205c854d
Chris Mathieson <chrismathieson25@gmail.com>
2021-04-05 20:35:36 -0400

    Create PowerCli.ps1

---
+++ EnterpriseVirtualization/PowerCli.ps1
@@ -0,0 +1,13 @@
+﻿$vServer = read_host -Prompt "Please enter host:"
+$user = read_host -Prompt "Please enter username:"
+$pass = read_host -Prompt "Please enter password:"
+
+function connect {
+    Connect-VIServer -Server $vServer -User $user -Password $pass
+}
+
+
+function listSpecs {
+
+    Get-OSCustomizationspec
+}
